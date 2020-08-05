d49e2ea31437ed7d782c0bf5a0b38b1af731d014
Harry Moore <harrymoore@yahoo.com>
2020-08-05 13:19:43 -0400

    updates to markdown alpaca field. use proper require/define to load extra module

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 define(function(require, exports, module) {
+    // var UI = require("ui");    
+    // window.markdownlint_moduleId = UI.extractModuleID(module.uri);
+    // require("./lib/markdownlint-browser.js");
     require("./fields/markdownlint-field.js");
 });
